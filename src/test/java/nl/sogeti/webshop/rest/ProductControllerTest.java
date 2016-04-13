124e771981d97461d47b6ed25fcb04117ac275b8
cuonto <cuong.to@sogeti.com>
2016-04-13 17:08:34 +0200

    Add Integration and Unit Tests

---
+++ src/test/java/nl/sogeti/webshop/rest/ProductControllerTest.java
@@ -0,0 +1,48 @@
+package nl.sogeti.webshop.rest;
+
+import nl.sogeti.webshop.Application;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+/**
+ * Created by cuong on 13-4-16.
+ * TODO: Fix NPE so that this is more an Unit Test than an Integration Test
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ProductControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getProducts() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/products").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"Test\",\"description\":\"Test description\",\"price\":1.95}]")));
+    }
+}
