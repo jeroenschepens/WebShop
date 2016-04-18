7406e0421e780c62ed8c4c071739d47381ac957e
Jeroen Schepens <jeroen.schepens@sogeti.com>
2016-04-18 16:39:19 +0200

    Refactored services

--- src/main/resources/public/js/services/cart.js
+++ src/main/resources/public/js/services/cart.js
@@ -4,43 +4,42 @@ angular.module('PetShop').factory('Cart', ['$localStorage', function ($localStor
         $localStorage.cart = {};
     }
 
-    var factory = {};
+    return {
 
-    factory.addItem = function (item) {
-        var amount = $localStorage.cart[item];
-        if (isNaN(amount)) {
-            $localStorage.cart[item] = 1;
-        } else {
-            $localStorage.cart[item] = amount + 1;
-        }
-    };
-
-    factory.removeItem = function (item) {
-        var amount = $localStorage.cart[item];
-        if (!isNaN(amount)) {
-            if (amount > 1) {
-                $localStorage.cart[item] = amount - 1;
+        addItem: function (item) {
+            var amount = $localStorage.cart[item];
+            if (isNaN(amount)) {
+                $localStorage.cart[item] = 1;
             } else {
-                delete $localStorage.cart[item];
+                $localStorage.cart[item] = amount + 1;
             }
+        },
+
+        removeItem: function (item) {
+            var amount = $localStorage.cart[item];
+            if (!isNaN(amount)) {
+                if (amount > 1) {
+                    $localStorage.cart[item] = amount - 1;
+                } else {
+                    delete $localStorage.cart[item];
+                }
+            }
+        },
+
+        countItems: function () {
+            var count = 0;
+            Object.keys($localStorage.cart).forEach(function (key) {
+                count = count + $localStorage.cart[key];
+            });
+            return count;
+        },
+
+        getItems: function () {
+            return $localStorage.cart;
+        },
+
+        clearItems: function () {
+            $localStorage.cart = {};
         }
-    };
-
-    factory.countItems = function () {
-        var count = 0;
-        Object.keys($localStorage.cart).forEach(function (key) {
-            count = count + $localStorage.cart[key];
-        });
-        return count;
-    };
-
-    factory.getItems = function () {
-        return $localStorage.cart;
-    };
-
-    factory.clearItems = function () {
-        $localStorage.cart = {};
-    };
-
-    return factory;
+    }
 }]);
