ede36cf08e54296f7a22781461e7ebf65c706aa2
Jeroen Schepens <jeroen.schepens@sogeti.com>
2016-04-19 22:55:06 +0200

    Added validation to house number and postcode

--- src/main/resources/public/js/controllers/place-controller.js
+++ src/main/resources/public/js/controllers/place-controller.js
@@ -1,8 +1,11 @@
 angular.module('PetShop').controller('PlaceController', ['$scope', '$location', 'Cart', function ($scope, $location, Cart) {
 
+    $scope.pcRegex = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i;
+    $scope.hnRegex = /^[1-9][0-9]{0,4} ?[a-z]{0,6}$/i;
+
     $scope.ready = false;
     $scope.id = null;
-    
+
     $scope.customer = {};
 
     $scope.placeOrder = function () {
@@ -10,7 +13,6 @@ angular.module('PetShop').controller('PlaceController', ['$scope', '$location', 
             Cart.clearItems();
             $scope.ready = true;
             $scope.id = data.id;
-        }).error(function (data) {
-        });
+        })
     };
 }]);
